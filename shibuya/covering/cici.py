c5131b103e71a23e8638882ad5c3eededae5380a
Parcly Taxel <reddeloostw@gmail.com>
2022-09-20 23:51:55 +0800

    Eleven Discs Problem

--- shibuya/covering/cici.py
+++ shibuya/covering/cici.py
@@ -6,6 +6,7 @@ Cf. https://erich-friedman.github.io/packing/circovcir
 """
 from mpmath import *
 from shibuya.draw import drawing
+from shibuya.generators import cu
 
 def thassqrt(q):
     return mpc(1-q,2*sqrt(q)) / (1+q)
@@ -33,11 +34,9 @@ def c5():
     with extraprec(50):
         r = polyroots([1296, 2112, -3480, 1360, 1665, -1776, 22, -800, 625])[2]
         a = polyroots([1296, -4576, -2232, 2552, 4261, -568, 7664, -3328, -1024])[1]
-        z1 = mpc(a-r-1, sqrt((r+a+1) * (3*r-a-1))) / 2 # mpc(b,c)
-        t2 = polyroots([100, -3304, 39337, -158014, 287575, -223724, 33895, -622, -75])[1]
-        u2 = polyroots([675, 918, -207, -5300, -5759, -250, 1007, 824, -100])[3]
-        z2 = (thassqrt(t2)+thassqrt(u2)) / 2
-    return (r, [a, z1, conj(z1), z2, conj(z2)])
+        t = cu(-1, a-r, r, r)
+        m = (cu(0, a, 1, r) + cu(t, 0, r, 1)) / 2
+    return (r, [a, t, m, conj(t), conj(m)])
 
 """
 LIB "elim.lib";
@@ -58,26 +57,34 @@ def c6():
         qp = [7841367, -33449976, 62607492, -63156942, 41451480, -19376280, 5156603, -746832, 54016, 3072]
         ap = [7841367, -90811125, 344734812, -376618374, -189418782, 373328980, 71968935, -130579181, 27056736, -800000]
         bp = [7841367, -7610085, -71679924, -45430974, 58085154, 26617188, -12289505, -5546269, 2814464, -327680]
-        z1xp = [335994734583, -925055302392, 1245681478080, -1737278445780, 1649211336432, -1213918522608, 517124784064, -136608531264, 17765003264, -663552000]
-        z1yp = [335994734583, -665599287231, 1897433047512, -2672527493250, 2452227909282, -430846892340, -903975402593, 814316672985, -232138895616, 7464960000]
-        z4xp = [282289212, -664669152, 1485276912, -2162565189, 2039760072, -1482840408, 758989690, -225747200, 20917924, -253125]
-        z4yp = [31365468, -117592236, 439727724, -870853095, 1390690842, 1186401779, -230544808, -235749633, 12250566, 11390625]
         r = sqrt(polyroots(qp)[1])
         a = -sqrt(polyroots(ap)[1])
         b = sqrt(polyroots(bp)[0])
-        z1x = -sqrt(polyroots(z1xp)[1])
-        z1y = sqrt(polyroots(z1yp)[2])
-        z1 = mpc(z1x,z1y)
-        z4x = sqrt(polyroots(z4xp)[1])
-        z4y = sqrt(polyroots(z4yp)[2])
-        z4 = mpc(z4x,z4y)
-    return (r, [a, b, z1, z4, conj(z1), conj(z4)])
+        t = cu(a, b, r, r) + cu(0, b, 1, r) - b
+        m = (cu(a, 0, r, 1) + cu(0, t, 1, r)) / 2
+    return (r, [a, b, t, m, conj(t), conj(m)])
 
 def c_rad(n):
     m = n-1
     r = 1 / (1 + 2*cospi(fraction(2,m)))
     d = 2*cospi(fraction(1,m)) * r
     return (r, [0] + [d*u for u in unitroots(m)])
+
+def c11():
+    with extraprec(100):
+        rp = [16, -56, 121, -458, 1709, -1773, -4406, 12770, -4789, -16653, 19746, -5805, -12708, 15406, -5897, 729]
+        ap = [704, 1040, 13239, 47050, 572272, 940099, 3697162, 42769344, 27697007, 178794966, 268623532, 23638520, -77077232, -1705056, 3613248, -279936]
+        r = polyroots(rp)[2]
+        a = polyroots(ap)[3]
+        p1 = cu(a+r, 1, r, r)
+        t = cu(0, p1, 1, r)
+        p2, v = t-r, t-p1
+        p3 = cu(0, p2, 1, r) + a+r - p1
+        b = 2*re(p3 - v) - a
+        c = -re(v) - sqrt(1 - im(v)**2)
+        p4 = cu(c, b, r, r) + v
+        ps = [p1, p2, p3, p4]
+    return (r, [a, b, c] + ps + [conj(p) for p in ps])
 
 def cn(n):
     """Return the best known circle covering of a unit circle
